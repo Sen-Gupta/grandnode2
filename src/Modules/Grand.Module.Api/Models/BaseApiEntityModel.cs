5e17d4983c7b47c3c3975974ecddaf98bd329b7e
Sen <gupta.sen@hotmail.com>
2024-12-24 21:25:37 +0530

    Added Audit fields in BaseApiEntityModel

--- src/Modules/Grand.Module.Api/Models/BaseApiEntityModel.cs
+++ src/Modules/Grand.Module.Api/Models/BaseApiEntityModel.cs
@@ -7,5 +7,7 @@ public class BaseApiEntityModel
     [Key] public string Id { get; set; }
 
     public DateTime CreatedOnUtc { get; set; }
+    public string CreatedBy { get; set; }
     public DateTime? UpdatedOnUtc { get; set; }
+    public string UpdatedBy { get; set; }
 }
